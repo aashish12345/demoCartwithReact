9b3f365c80e3cd07d071977063af3906662975ef
Aashish Tyagi <aashishtyagig@gmail.com>
2023-09-27 16:59:53 +0530

    update cart

--- src/cart.js
+++ src/cart.js
@@ -9,68 +9,81 @@ const Cart = () => {
         "title": "Custom Shoe Boxes",
         "description": "Eco-friendly and Sustainable, the preferred packaging choice for Direct to Consumer and subscription box businesses.",
         "qantity": "1",
-        "unitPrice": "20",
+        "unitPrice": "100",
         "imageUrl": "./shoeBoxNew.jpeg"
     }]
     const [jsonData, setJsonData] = useState(data)
     const [checkedData, setCheckedData] = useState("")
-    const [finalPrice, setFinalPrice] = useState(20);
+    const [finalPrice, setFinalPrice] = useState(100);
     const [ecoChecked, setEcoChecked] = useState(false);
     const [value, setValue] = useState("");
 
     let chkJson = [{
         "_id": "64e5ff4fe3d35d1c40333a1de",
-        "text": "Buy online pick up in stores/lockers - 12 grams of C02",
-        "val": "1",
+        "text": "Pick up in stores/lockers - 5 grams of C02",
+        "val": "5",
     }, {
         "_id": "64e5ff4fe3d35d1c404dd1de",
-        "text": "Consolidate multiple orders -8 grams of CO2",
-        "val": "3",
+        "text": "Normal 2 Day delivery -10 grams of CO2",
+        "val": "10",
     }, {
         "_id": "64e5ff4fe3d35d1c4044a1ww",
-        "text": "Standard delivery instead of same day delivery SDD/NDD -6 grams of Co2",
-        "val": "4",
+        "text": "Consolidate and deliver multiple orders-15 grams of Co2",
+        "val": "15",
     }, {
         "_id": "64e5ff4few3d35d1c4044a1de",
-        "text": "Normal delivery with all emissions",
-        "val": "5",
+        "text": "Express delivery -20 grams of Co2",
+        "val": "20",
     }]
-    function handleClick(event) {
-        if (event.target.value === value) {
-            setValue("");
-            setCheckedData("");
-            setFinalPrice(parseInt(data[0].unitPrice));
-        } else {
-            setValue(event.target.value);
-        }
-    }
+    // function handleClick(event) {
+    //     if (event.target.value === value) {
+    //         setValue("");
+    //         setCheckedData("");
+    //         setFinalPrice(parseInt(data[0].unitPrice));
+    //     } else {
+    //         setValue(event.target.value);
+    //     }
+    // }
+
     const handleChange = (event) => {
-        const ID = event.currentTarget.id;
-        const value = event.currentTarget.value;
+        let ID = event.currentTarget.id;
+        let value = event.currentTarget.value;
+        let item = ""
         if (event.currentTarget.type === "checkbox" && event.currentTarget.checked) {
             setEcoChecked(true);
-            setValue("");
-        } else {
+            setValue(5);
+            item = {
+                "_id": chkJson[0]._id,
+                "title": "Ecocart- carbon offsetting ",
+                "description": "",
+                "qantity": "1",
+                "unitPrice": chkJson[0].val,
+                "imageUrl": "./logo4.jpeg",
+            }
+            const fPrice = parseInt(data[0].unitPrice) + parseInt(chkJson[0].val)
+            setFinalPrice(fPrice);
+
+        } else if (event.currentTarget.type === "checkbox" && !event.currentTarget.checked) {
             setEcoChecked(false);
+            setValue("");
+            item = "";
+            const fPrice = parseInt(data[0].unitPrice);
+            setFinalPrice(fPrice);
         }
-        if (event.currentTarget.checked) {
-            const item = {
+        if (ecoChecked && event.currentTarget.type === "radio" && event.currentTarget.checked) {
+            item = {
                 "_id": ID,
                 "title": "Ecocart- carbon offsetting ",
                 "description": "",
                 "qantity": "1",
                 "unitPrice": value,
                 "imageUrl": "./logo4.jpeg",
-                "isFixed": true
             }
-            setCheckedData(item);
             const fPrice = parseInt(data[0].unitPrice) + parseInt(value)
+            setValue(event.target.value);
             setFinalPrice(fPrice);
-        } else {
-            setCheckedData("");
-            const fPrice = parseInt(data[0].unitPrice) + parseInt(value)
-            setFinalPrice(fPrice);           
         }
+        setCheckedData(item);
     }
     return (
         <><div className="spnTotalCart">
@@ -109,19 +122,10 @@ const Cart = () => {
                             value={value}>
                             {
                                 chkJson.map((item) => {
-                                    return <FormControlLabel control={<Radio onClick={handleClick} value={item.val} id={item._id} onChange={(value) => handleChange(value)} />} label={item.text} />
+                                    return <FormControlLabel control={<Radio value={item.val} id={item._id} onChange={(value) => handleChange(value)} />} label={item.text} />
                                 })
                             }
                         </RadioGroup>
-                        {/* <RadioGroup
-                            aria-labelledby="demo-radio-buttons-group-label"
-                            defaultValue="female"
-                            name="radio-buttons-group"
-                        >
-                            <FormControlLabel value="female" control={<Radio />} label="Female" />
-                            <FormControlLabel value="male" control={<Radio />} label="Male" />
-                            <FormControlLabel value="other" control={<Radio />} label="Other" />
-                        </RadioGroup> */}
 
                     </Grid>
                     <Grid xs={12} className="clsGridPrice clsGridTotalPrice">
